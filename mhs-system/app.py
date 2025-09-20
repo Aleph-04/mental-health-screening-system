2b886b124e69d45f6165685f909380db4e825eb9
Aleph-04 <lorenzopina22@gmail.com>
2025-09-20 21:43:33 +0800

    fixed static file directories

--- mhs-system/app.py
+++ mhs-system/app.py
@@ -14,5 +14,9 @@ def login():
 def dashboard():
     return render_template("index.html")
 
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
