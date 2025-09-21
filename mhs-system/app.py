a231f9ccd1b986892426451c9ed76feeb01d0293
Myke Kieran Alfane <mykekierkier@gmail.com>
2025-09-22 00:42:46 +0800

    adjusted files connects buttons from view and edit forms

--- mhs-system/app.py
+++ mhs-system/app.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
-
-@app.route("/login.html")
-def login():
     return render_template("login.html")
 
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route("/charts.html")
-def charts():
-    return render_template("charts.html")
+@app.route("/stats")
+def stats():
+    return render_template("statistics.html")
 
 @app.route("/edit-evaluation-form")
 def edit_evaluation_form():
     return render_template("edit-evaluation-form.html")
+
+@app.route("/view-forms")
+def view_forms():
+    return render_template("view-forms.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
