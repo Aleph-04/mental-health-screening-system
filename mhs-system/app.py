9c5a0f5754dc7b06e279c68407d14f26522a7749
Aleph-04 <lorenzopina22@gmail.com>
2025-09-22 15:16:42 +0800

    student side

--- mhs-system/app.py
+++ mhs-system/app.py
@@ -4,27 +4,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("login.html")
+    return render_template("admin_login.html")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("admin_dashboard.html")
 
 @app.route("/stats")
 def stats():
-    return render_template("statistics.html")
+    return render_template("admin_statistics.html")
 
 @app.route("/edit-evaluation-form")
 def edit_evaluation_form():
-    return render_template("edit-evaluation-form.html")
+    return render_template("admin_edit-evaluation-form.html")
 
 @app.route("/view-forms")
 def view_forms():
-    return render_template("view-forms.html")
+    return render_template("admin_view-forms.html")
 
 @app.route("/results")
 def results():
-    return render_template("results.html")
+    return render_template("admin_results.html")
+
+@app.route("/evaluation")
+def student_evaluation():
+    return render_template("student_evaluation_form.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
