80af97d1bb8c5c6f7302982130dced4f0a12806d
kieralfane22 <mykekierkier@gmail.com>
2025-11-25 21:55:40 +0800

    Check lang Lor

--- mhs-system/app.py
+++ mhs-system/app.py
@@ -45,15 +45,39 @@ def student_evaluation():
     return render_template("student_evaluation_form.html")
 
 ### example for database insertion boi; once mag submit ang forms, ga run ang function nga ja.###
-@app.route("/evaluation", methods=['POST'])
+@app.route("/evaluation", methods=['GET', 'POST'])
 def submit_to_database():
     first_name = request.form['inputFirstName']
     middle_name = request.form['inputMiddleName']
     last_name = request.form['inputLastName']
     email_address = request.form['inputEmailAddress']
+
+    ### PHQ9
+    phq1 = int(request.form.get('phq1', 0))
+    phq2 = int(request.form.get('phq2', 0))
+    phq3 = int(request.form.get('phq3', 0))
+    phq4 = int(request.form.get('phq4', 0))
+    phq5 = int(request.form.get('phq5', 0))
+    phq6 = int(request.form.get('phq6', 0))
+    phq7 = int(request.form.get('phq7', 0))
+    phq8 = int(request.form.get('phq8', 0))
+    phq9 = int(request.form.get('phq9', 0))
+
+    ### GAD7
+    gad1 = int(request.form.get('gad1', 0))
+    gad2 = int(request.form.get('gad2', 0))
+    gad3 = int(request.form.get('gad3', 0))
+    gad4 = int(request.form.get('gad4', 0))
+    gad5 = int(request.form.get('gad5', 0))
+    gad6 = int(request.form.get('gad6', 0))
+    gad7 = int(request.form.get('gad7', 0))
     
     ### function imported from database.py ###
-    insert_to_db(first_name, middle_name, last_name, email_address)
+    insert_to_db(
+            first_name, middle_name, last_name, email_address,
+            phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
+            gad1, gad2, gad3, gad4, gad5, gad6, gad7
+        )
     return redirect(url_for("student_evaluation"))
     
 
--- mhs-system/database.py
+++ mhs-system/database.py
@@ -11,21 +11,40 @@ def initialize_db():
             "middle_name"	TEXT,
             "last_name"	TEXT,
             "email_address"	TEXT,
+            "phq1" INTEGER,
+            "phq2" INTEGER,
+            "phq3" INTEGER,
+            "phq4" INTEGER,
+            "phq5" INTEGER,
+            "phq6" INTEGER,
+            "phq7" INTEGER,
+            "phq8" INTEGER,
+            "phq9" INTEGER,
+            "gad1" INTEGER,
+            "gad2" INTEGER,
+            "gad3" INTEGER,
+            "gad4" INTEGER,
+            "gad5" INTEGER,
+            "gad6" INTEGER,
+            "gad7" INTEGER
             PRIMARY KEY("id" AUTOINCREMENT)
         )
     ''')
     
     print("Hello from database.py")
     
-def insert_to_db(first_name, middle_name, last_name, email_address):
+def insert_to_db(first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
+                  gad1, gad2, gad3, gad4, gad5, gad6, gad7):
     conn = sqlite3.connect('database.db')  
     cursor = conn.cursor()                      
 
     cursor.execute(
-        """INSERT INTO responses (first_name, middle_name, last_name, email_address)
+        """INSERT INTO responses (first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9)
            VALUES (?, ?, ?, ?)""",
-        (first_name, middle_name, last_name, email_address)  
+        (first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9, gad1, gad2, gad3, gad4, gad5, gad6, gad7)  
     )
+
+
 
     conn.commit() 
     conn.close()  
