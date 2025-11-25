817c15af086c760817ce5cbdbafe7c903b06afcc
kieralfane22 <mykekierkier@gmail.com>
2025-11-25 22:40:35 +0800

    check database.py

--- mhs-system/database.py
+++ mhs-system/database.py
@@ -5,47 +5,53 @@ def initialize_db():
     cursor = conn.cursor()
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS "responses" (
-            "id"	INTEGER,
-            "first_name"	TEXT,
-            "middle_name"	TEXT,
-            "last_name"	TEXT,
-            "email_address"	TEXT,
-            "phq1" INTEGER,
-            "phq2" INTEGER,
-            "phq3" INTEGER,
-            "phq4" INTEGER,
-            "phq5" INTEGER,
-            "phq6" INTEGER,
-            "phq7" INTEGER,
-            "phq8" INTEGER,
-            "phq9" INTEGER,
-            "gad1" INTEGER,
-            "gad2" INTEGER,
-            "gad3" INTEGER,
-            "gad4" INTEGER,
-            "gad5" INTEGER,
-            "gad6" INTEGER,
-            "gad7" INTEGER
-            PRIMARY KEY("id" AUTOINCREMENT)
+        CREATE TABLE IF NOT EXISTS responses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT,
+            middle_name TEXT,
+            last_name TEXT,
+            email_address TEXT,
+            phq1 INTEGER,
+            phq2 INTEGER,
+            phq3 INTEGER,
+            phq4 INTEGER,
+            phq5 INTEGER,
+            phq6 INTEGER,
+            phq7 INTEGER,
+            phq8 INTEGER,
+            phq9 INTEGER,
+            gad1 INTEGER,
+            gad2 INTEGER,
+            gad3 INTEGER,
+            gad4 INTEGER,
+            gad5 INTEGER,
+            gad6 INTEGER,
+            gad7 INTEGER
         )
     ''')
-    
+
     print("Hello from database.py")
-    
-def insert_to_db(first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
-                  gad1, gad2, gad3, gad4, gad5, gad6, gad7):
-    conn = sqlite3.connect('database.db')  
-    cursor = conn.cursor()                      
-
-    cursor.execute(
-        """INSERT INTO responses (first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9)
-           VALUES (?, ?, ?, ?)""",
-        (first_name, middle_name, last_name, email_address, phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9, gad1, gad2, gad3, gad4, gad5, gad6, gad7)  
-    )
+    conn.close()
 
 
+def insert_to_db(first_name, middle_name, last_name, email_address,
+                 phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
+                 gad1, gad2, gad3, gad4, gad5, gad6, gad7):
 
-    conn.commit() 
-    conn.close()  
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
 
+    cursor.execute("""
+        INSERT INTO responses (
+            first_name, middle_name, last_name, email_address,
+            phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
+            gad1, gad2, gad3, gad4, gad5, gad6, gad7
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    """, (
+        first_name, middle_name, last_name, email_address,
+        phq1, phq2, phq3, phq4, phq5, phq6, phq7, phq8, phq9,
+        gad1, gad2, gad3, gad4, gad5, gad6, gad7
+    ))
+
+    conn.commit()
+    conn.close()
